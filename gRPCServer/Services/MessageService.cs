e7388a65b7aac2c42dcf030aaf05e3711014f8c2
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-08 16:57:45 +0300

    Stream Type

--- gRPCServer/Services/MessageService.cs
+++ gRPCServer/Services/MessageService.cs
@@ -11,14 +11,27 @@ namespace gRPCServer.Services
         {
             _logger = logger;
         }
-
-        public override  Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        //unary type
+        public override Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
         {
             Console.WriteLine("grpc requesti alındı");
             return Task.FromResult(new MessageResponse
             {
-                Message= "Hello " + request.Name
+                Message = "Hello " + request.Name
             });
+        }
+        //stream type
+        public override async Task SendStreamMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"Message : {request.Message} | Name : {request.Name}");
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(new MessageResponse
+                {
+                    Message = "Hello " + request.Name + "-response"
+                }) ;
+            }
         }
 
 
--- gRPC_Client/Program.cs
+++ gRPC_Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using gRPCMessageClient;
 using gRPCServer;
 
@@ -7,24 +8,41 @@ class Program
     static async Task Main(string[] args)
     {
         var channel = GrpcChannel.ForAddress("https://localhost:7194");
+        ///////// UNARY //////////
+
+        //unary type example
         //var greetClient = new Greeter.GreeterClient(channel);
         //Thread.Sleep(5000);
         //HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "gRPC ilk request" });
 
+        //unary type example
+        //var messageClient = new Message.MessageClient(channel);
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    MessageResponse result = await messageClient.SendMessageAsync(new MessageRequest
+        //    {
+        //        Name = "İbrahim",
+        //        Message = "Merhaba"
+        //    });
+        //    Thread.Sleep(5000);
+        //    Console.WriteLine(result.Message);
+        //}
+
+        ///////// STREAM //////////
+
+        //stream type example
         var messageClient = new Message.MessageClient(channel);
-       
-        
-        for (int i = 0; i < 5; i++)
+        var response = messageClient.SendStreamMessage(new MessageRequest
         {
-            MessageResponse result = await messageClient.SendMessageAsync(new MessageRequest
-            {
-                Name = "İbrahim",
-                Message = "Merhaba"
-            });
-            Thread.Sleep(5000);
-            Console.WriteLine(result.Message);
+            Name = "İbrahim",
+            Message = "Merhaba"
+        });
+        CancellationTokenSource cts = new();
+        while (await response.ResponseStream.MoveNext(cts.Token))
+        {
+            Console.WriteLine(response.ResponseStream.Current.Message);
         }
-        
+
         Console.ReadLine();
     }
 }

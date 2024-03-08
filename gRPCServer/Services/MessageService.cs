46d6c9c1b8f41cfae64b08af993483508c70bb51
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-09 00:12:36 +0300

    client/bi-directional type

--- gRPCServer/Services/MessageService.cs
+++ gRPCServer/Services/MessageService.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Core;
 using gRPCMessageServer;
 using gRPCServer;
+using System.IO;
 
 namespace gRPCServer.Services
 {
@@ -20,8 +21,8 @@ namespace gRPCServer.Services
                 Message = "Hello " + request.Name
             });
         }
-        //stream type
-        public override async Task SendStreamMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        //server stream type
+        public override async Task SendServerStreamMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
         {
             Console.WriteLine($"Message : {request.Message} | Name : {request.Name}");
             for (int i = 0; i < 10; i++)
@@ -30,10 +31,45 @@ namespace gRPCServer.Services
                 await responseStream.WriteAsync(new MessageResponse
                 {
                     Message = "Hello " + request.Name + "-response"
-                }) ;
+                });
             }
         }
 
+        //client stream type
+        public override async Task<MessageResponse> SendClientStreamMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext(context.CancellationToken))
+            {
+                Console.WriteLine($"Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+            }
+            return new MessageResponse
+            {
+                Message = "Veri Alınmıştır"
+            };
+        }
+
+        //bi-directional stream type
+        public override async Task SendBiDirectionalStreamMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            var task1 = Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext(context.CancellationToken))
+                {
+                    Console.WriteLine($"Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+                }
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(new MessageResponse
+                {
+                    Message = "Mesaj " + i
+                });
+            }
+
+            await task1;
+        }
 
     }
 }
--- gRPC_Client/Program.cs
+++ gRPC_Client/Program.cs
@@ -10,12 +10,12 @@ class Program
         var channel = GrpcChannel.ForAddress("https://localhost:7194");
         ///////// UNARY //////////
 
-        //unary type example
+        ////unary type example
         //var greetClient = new Greeter.GreeterClient(channel);
         //Thread.Sleep(5000);
         //HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "gRPC ilk request" });
 
-        //unary type example
+        ////unary type example
         //var messageClient = new Message.MessageClient(channel);
         //for (int i = 0; i < 5; i++)
         //{
@@ -28,20 +28,79 @@ class Program
         //    Console.WriteLine(result.Message);
         //}
 
-        ///////// STREAM //////////
+        ///////// UNARY //////////
 
-        //stream type example
+
+        ///////// SERVER STREAM //////////
+
+        ////server stream type example
+        //var messageClient = new Message.MessageClient(channel);
+        //var response = messageClient.SendServerStreamMessage(new MessageRequest
+        //{
+        //    Name = "İbrahim",
+        //    Message = "Merhaba"
+        //});
+        //CancellationTokenSource cts = new();
+        //while (await response.ResponseStream.MoveNext(cts.Token))
+        //{
+        //    Console.WriteLine(response.ResponseStream.Current.Message);
+        //}
+
+        ///////// SERVER STREAM //////////
+
+
+        ///////// CLIENT STREAM //////////
+
+        ////client stream type example
+        //var messageClient = new Message.MessageClient(channel);
+        //var request = messageClient.SendClientStreamMessage();
+        //for (int i = 0; i < 10; i++)
+        //{
+        // await Task.Delay(2000);
+        //    await request.RequestStream.WriteAsync(new MessageRequest
+        //    {
+        //        Message = $"{i} . İstek server'a gönderildi",
+        //        Name="İbrahim Demircik"
+
+        //    });
+        //}
+
+        ////stream datanın sonlandığınıı ifade eder.
+        //await request.RequestStream.CompleteAsync();
+        //await Console.Out.WriteLineAsync((await request.ResponseAsync).Message);
+
+        ///////// CLIENT STREAM //////////
+
+
+        ///////// BI-DIRECTİONAL STREAM //////////
+
+        ////bi-directional stream type example
         var messageClient = new Message.MessageClient(channel);
-        var response = messageClient.SendStreamMessage(new MessageRequest
+        var request = messageClient.SendBiDirectionalStreamMessage();
+        var task1= Task.Run(async () =>
         {
-            Name = "İbrahim",
-            Message = "Merhaba"
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(2000);
+                await request.RequestStream.WriteAsync(new MessageRequest
+                {
+                    Message = $"{i} . İstek server'a gönderildi",
+                    Name = "İbrahim Demircik"
+
+                });
+            }
         });
+
         CancellationTokenSource cts = new();
-        while (await response.ResponseStream.MoveNext(cts.Token))
+        while (await request.ResponseStream.MoveNext(cts.Token))
         {
-            Console.WriteLine(response.ResponseStream.Current.Message);
+            Console.WriteLine(request.ResponseStream.Current.Message);
         }
+
+        await task1;
+        await request.RequestStream.CompleteAsync();
+
+        ///////// BI-DIRECTIONAL STREAM //////////
 
         Console.ReadLine();
     }

aa22544fd845b166f0a3f504771114df54425210
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-08 15:57:21 +0300

    Add project files.

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using gRPCServer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var channel = GrpcChannel.ForAddress("https://localhost:7194");
+        var greetClient = new Greeter.GreeterClient(channel);
+        Thread.Sleep(5000);
+        HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "gRPC ilk request" });
+        Console.WriteLine(result.Message);
+        Console.ReadLine();
+    }
+}

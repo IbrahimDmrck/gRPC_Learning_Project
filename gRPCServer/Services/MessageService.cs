7f5e8f1e085fb8fc19001f04fcda5a1ff92032b3
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-08 16:37:13 +0300

    Unary Type

---
+++ gRPCServer/Services/MessageService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using gRPCMessageServer;
+using gRPCServer;
+
+namespace gRPCServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public MessageService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override  Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("grpc requesti alındı");
+            return Task.FromResult(new MessageResponse
+            {
+                Message= "Hello " + request.Name
+            });
+        }
+
+
+    }
+}
--- gRPC_Client/Program.cs
+++ gRPC_Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Net.Client;
+using gRPCMessageClient;
 using gRPCServer;
 
 class Program
@@ -6,10 +7,24 @@ class Program
     static async Task Main(string[] args)
     {
         var channel = GrpcChannel.ForAddress("https://localhost:7194");
-        var greetClient = new Greeter.GreeterClient(channel);
-        Thread.Sleep(5000);
-        HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "gRPC ilk request" });
-        Console.WriteLine(result.Message);
+        //var greetClient = new Greeter.GreeterClient(channel);
+        //Thread.Sleep(5000);
+        //HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "gRPC ilk request" });
+
+        var messageClient = new Message.MessageClient(channel);
+       
+        
+        for (int i = 0; i < 5; i++)
+        {
+            MessageResponse result = await messageClient.SendMessageAsync(new MessageRequest
+            {
+                Name = "İbrahim",
+                Message = "Merhaba"
+            });
+            Thread.Sleep(5000);
+            Console.WriteLine(result.Message);
+        }
+        
         Console.ReadLine();
     }
 }
